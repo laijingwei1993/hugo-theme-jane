fb3c08c61afddd9a5e8a08bd2549299bd5e98ede
赖经纬 <laijingwei1993@163.com>
2020-04-18 00:26:15 +0800

    2020/04/18 周六  0:26:14.20 LJW

---
+++ static/js/share.js
@@ -0,0 +1,41 @@
+$(function () {
+  var title = document.title.substring(0, document.title.length - 6);
+  var imgUrl = $("img:first").data('src') || location.origin + '/favicon.ico';
+  var desc = $("meta[name='description']").attr('content');
+
+  var shareobj = {
+    title: title,
+    desc: desc,
+    link: location.href,
+    imgUrl: imgUrl
+  };
+
+  var timeObj = {
+    title: title,
+    link: location.href,
+    imgUrl: imgUrl
+  }
+
+  var url = location.origin + "/api/jsapi_signature?&url=" + encodeURIComponent(location.href);
+
+  var ua = navigator.userAgent.toLowerCase();
+  var isWeixin = ua.indexOf('micromessenger') != -1;
+  if (isWeixin) {
+    $.getJSON(url, function (res) {
+      wx.config({
+        debug: false,
+        appId: res.appId,
+        timestamp: res.timestamp,
+        nonceStr: res.nonceStr,
+        signature: res.signature,
+        jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline']
+      });
+
+      wx.ready(function () {
+        wx.onMenuShareAppMessage(shareobj);
+        wx.onMenuShareTimeline(timeObj);
+      });
+    });
+  }
+
+});
